e5546e27472d21902192bc2baeae5c0ae5d9e493
Negus <natnaelbirhanu22@gmail.com>
2024-04-11 19:29:09 +0300

    experiments

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,12 +1,17 @@
 {{-- <a {{ $attributes }}>
     {{ $slot}}
 </a> --}}
-@props(['active' => false])
+@props(['active' => false]);
+{{-- 
+@php
+$active = request()->is('about');
+if($active) {
+    dd($active);
+}
+@endphp --}}
 
 
-
-<a class="{{ $active ? " bg-gray-900 text-white" :"text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-base font-medium" aria-current="{{ $active ? 'page' : 'false'}}"
-    {{$attributes}}
-    >
+<a class="{{ $active ? " bg-gray-900 text-white" :"text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3
+    py-2 text-base font-medium" aria-current="{{ $active ? 'page' : 'false'}}" {{$attributes}}>
     {{ $slot }}
 </a>
