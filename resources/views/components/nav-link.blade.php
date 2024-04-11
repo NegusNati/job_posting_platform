42a0577002748ec7a23828a2cc1ce68ba40f7899
Negus <natnaelbirhanu22@gmail.com>
2024-04-11 20:58:16 +0300

    clean up

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
 {{-- <a {{ $attributes }}>
     {{ $slot}}
 </a> --}}
-@props(['active' => false , 'type' => 'a']);
+@props(['active' => false ]);
 {{--
 @php
 $active = request()->is('about');
@@ -10,23 +10,7 @@ dd($active);
 }
 @endphp --}}
 
-
-@if ( $type == 'button')
-
-<button class="{{ $active ? " bg-gray-900 text-white" :"text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md
-    px-3 py-2 text-base font-medium" aria-current="{{ $active ? 'page' : 'false'}}"
-    onclick="window.location.href='{{ $attributes['href']}}'">
-    {{ $slot }}
-
-</button>
-
-
-@else
-
 <a class="{{ $active ? " bg-gray-900 text-white" :"text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3
     py-2 text-base font-medium" aria-current="{{ $active ? 'page' : 'false'}}" {{$attributes}}>
     {{ $slot }}
-
 </a>
-
-@endif
