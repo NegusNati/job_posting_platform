dacfe52a4fa779ba761ef374ea157e800eced661
Negus <natnaelbirhanu22@gmail.com>
2024-04-17 23:29:48 +0300

    delete functionality

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -1,7 +1,6 @@
-
 <x-layout>
     <x-slot:heading>
-       Edit Job: {{$job->title}}
+        Edit Job: {{$job->title}}
     </x-slot:heading>
 
     <!--
@@ -31,11 +30,11 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title" autocomplete="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Software Engineer" required value="{{$job['title']}}" >
+                                    placeholder="Software Engineer" required value="{{$job['title']}}">
                             </div>
                         </div>
                         @error('title')
-                            <p class='text-xs text-red-500 italic mt-1'>{{$message}}</p>
+                        <p class='text-xs text-red-500 italic mt-1'>{{$message}}</p>
                         @enderror
                     </div>
                     <div class="sm:col-span-4">
@@ -50,7 +49,7 @@
                             </div>
                         </div>
                         @error('salary')
-                            <p class='text-xs text-red-500 mt-1 italic'>{{$message}}</p>
+                        <p class='text-xs text-red-500 mt-1 italic'>{{$message}}</p>
                         @enderror
                     </div>
                     <div class="col-span-full">
@@ -58,14 +57,15 @@
                             class="block text-sm font-medium leading-6 text-gray-900">Description</label>
                         <div class="mt-2">
                             <textarea id="description" name="description" rows="3"
-                                class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required >{{$job['description']}}</textarea>
+                                class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                required>{{$job['description']}}</textarea>
                         </div>
                         <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences describing the Job, and
                             related
                             benefits.</p>
 
                         @error('description')
-                            <p class='text-red-500 italic text-xs mt-1' >{{$message}}</p>
+                        <p class='text-red-500 italic text-xs mt-1'>{{$message}}</p>
                         @enderror
                     </div>
 
@@ -82,11 +82,29 @@
                     @endif
                 </div> --}}
 
-                <div class="mt-6 flex items-center justify-end gap-x-6">
-                    <a href="/jobs/{{ $job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-                    <button type="submit"
-                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+
+
+                <div class="mt-6 flex items-center justify-between gap-x-6">
+
+                    <div class="flex items-center">
+                        <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+                    </div>
+
+                    <div class="flex items-center gap-x-6">
+                        <a href="/jobs/{{ $job->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                        <div>
+                            <button type="submit"
+                                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div>
+    </form>
+
+    <form method="POST" action="/jobs/{{$job->id}}" class="hidden" id="delete-form" >
+        @csrf
+        @method("DELETE")
     </form>
 
 </x-layout>
